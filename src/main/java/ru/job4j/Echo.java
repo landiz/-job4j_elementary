78bb7d32692a9d2b2810d78870e790798f3730e0
ayazev <yazev73@gmail.com>
2022-03-20 21:55:24 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,4 +1,14 @@
 package ru.job4j;
 
 public class Echo {
+    int age = 27;
+    char gender = 'M';
+    float height = 1.76F;
+    short amountBricks = 32000;
+    double amountBacteria = 100000000;
+    long distance = 12312234123123L;
+    boolean workPermit = false;
+    float fileSize = 12.5F;
+
+
 }
